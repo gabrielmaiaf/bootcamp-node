f9eb50cba16ee3da820e611ce856b233abad37aa
Gabriel Maia <gabrielmaia@protonmail.com>
2020-09-01 16:39:17 -0300

    Add Appointment and Users

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider_id, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return res.json(appointment);
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message,
+    });
+  }
+});
+
+export default appointmentsRouter;
